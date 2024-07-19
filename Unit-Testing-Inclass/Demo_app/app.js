831054fc496d77f668ad789338536e7573fba3a0
Vedant Patel <86098072+VedantPatel-1905@users.noreply.github.com>
2024-07-19 10:47:50 -0400

    completed in-class demo

--- Unit-Testing-Inclass/Demo_app/app.js
+++ Unit-Testing-Inclass/Demo_app/app.js
@@ -2,6 +2,14 @@ function add(a,b){
     return a + b;
 }
 
+function welcomeMessage(){
+    return "Hello World!"; 
+}
+
+function checkEquality(a,b){
+    return a == b
+}
+
 module.exports = {
-    add
+    add, welcomeMessage, checkEquality
 }
--- Unit-Testing-Inclass/test/test.js
+++ Unit-Testing-Inclass/test/test.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
 const add = require('../Demo_app/app').add;
+const welcomeMessage = require('../Demo_app/app').welcomeMessage;
+const checkEquality = require('../Demo_app/app').checkEquality;
 
 describe('Testing add function', () => {
     it('testing to add 1 and 7', () => {
@@ -9,5 +11,23 @@ describe('Testing add function', () => {
     it('testing to add 4 and 7', () => {
         const result = add(4,7);
         assert.equal(result,11);
+    });
+});
+
+describe("Welcome message function", () => {
+    it("testing weather correct greeting is displayed", () => {
+        const result = welcomeMessage();
+        assert.equal(result,"Hello World!");
+    });
+});
+
+describe("Check Equality function", () => {
+    it("testing weather number 2 is equal to 2", () =>{
+        const result = checkEquality(2,2);
+        assert.equal(result,true);
+    });
+    it("testing weather string 'two' is equal to 'two", () =>{
+        const result = checkEquality("two","two");
+        assert.equal(result,true);
     });
 });
